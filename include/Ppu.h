ff5acf19f22347f709cdbd9b012fb7ecce436a7a
Ravi <trivediravi561@gmail.com>
2025-08-23 16:25:06 -0400

    setup ppu memory functions

--- include/Ppu.h
+++ include/Ppu.h
@@ -5,13 +5,43 @@
 
 class PPU
 {
+  struct OAM_ENTRY
+  {
+    uint8_t y;
+    uint8_t x;
+    uint8_t tile;
+    union
+    {
+      struct
+      {
+        uint8_t cgbPn : 3;
+        uint8_t cgbVramBank : 1;
+        uint8_t pn : 1;
+        uint8_t xFlip : 1;
+        uint8_t yFlip : 1;
+        uint8_t bgp : 1;
+      };
+      uint8_t flags;
+    };
+  };
+
   struct State
   {
+    std::array<OAM_ENTRY, 40> oamRam;
     std::array<uint8_t, 0x2000> vram;
   };
 
 public:
   void tick();
+
 private:
   State state;
+
+  static constexpr uint16_t VRAM_START_ADDR = 0x8000;
+  static constexpr uint16_t OAM_START_ADDR = 0xFE00;
+
+  void oamWrite(uint16_t addr, uint8_t value);
+  uint8_t oamRead(uint16_t addr);
+  uint8_t vramRead(uint16_t address) const;
+  void vramWrite(uint16_t address, uint8_t value);
 };
--- src/lib/ppu/Ppu.cpp
+++ src/lib/ppu/Ppu.cpp
@@ -2,4 +2,35 @@
 
 void PPU::tick()
 {
-} 
+}
+
+
+void PPU::oamWrite(uint16_t address, uint8_t value)
+{
+  if (address >= OAM_START_ADDR)
+  {
+    address -= OAM_START_ADDR;
+  }
+  uint8_t *p = reinterpret_cast<uint8_t *>(state.oamRam.data());
+  p[address] = value;
+}
+
+uint8_t PPU::oamRead(uint16_t address)
+{
+  if (address >= OAM_START_ADDR)
+  {
+    address -= OAM_START_ADDR;
+  }
+  uint8_t *p = reinterpret_cast<uint8_t *>(state.oamRam.data());
+  return p[address];
+}
+
+void PPU::vramWrite(uint16_t address, uint8_t value)
+{
+  state.vram[address - VRAM_START_ADDR] = value;
+}
+
+uint8_t PPU::vramRead(uint16_t address) const
+{
+  return state.vram[address - VRAM_START_ADDR];
+}
