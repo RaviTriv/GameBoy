001818515679cba85f2198945ccd1ec9bad0eb25
Ravi <trivediravi561@gmail.com>
2025-08-25 20:36:28 -0400

    fix header

--- include/Pipeline.h
+++ include/Pipeline.h
@@ -49,5 +49,5 @@ private:
   uint8_t calculateMapX() const;
   uint8_t calculateMapY() const;
   uint8_t calculateTileY() const;
-  uint32_t Pipeline::bufferIndex() const;
+  uint32_t bufferIndex() const;
 };
