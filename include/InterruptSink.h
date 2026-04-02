8d9608af2cefb7e79dad3235a5ea970958d6f33e
Ravi <trivediravi561@gmail.com>
2026-04-02 19:04:54 -0400

    eat interrupts

--- include/InterruptSink.h
+++ include/InterruptSink.h
@@ -1,6 +1,15 @@
 #pragma once
 
-#include "Interrupts.h"
+#include <cstdint>
+
+enum class InterruptType
+{
+  VBLANK = 1,
+  LCD_STAT = 2,
+  TIMER = 4,
+  SERIAL = 8,
+  JOYPAD = 16
+};
 
 class InterruptSink
 {
